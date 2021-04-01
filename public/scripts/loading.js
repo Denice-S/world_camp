41055bb1442311d8672dbfc0f621790d837ae6f9
Denice Soper <denice.soper@gmail.com>
2021-04-01 13:31:03 +0100

    added a preloader

---
+++ public/scripts/loading.js
@@ -0,0 +1,6 @@
+const spinner=document.getElementById("spinner");
+const button=document.getElementById("preloaderbutton")
+
+button.addEventListener("click", function () {
+    spinner.style.display="block"
+})
